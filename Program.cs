40816889b4508106922475e8e1b40874247cbe0a
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 21:40:58 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Pesquisa-binaria
+{
+    internal class Program
+    {
+        static int pesquisaBinaria(int[] vetor, int item, int inicio, int fim)
+        {
+            if (inicio <= fim)
+            {
+                int meio = (inicio + fim) / 2;
+                if (vetor[meio] == item)
+                    return meio;
+                if (vetor[meio] > item)
+                    return pesquisaBinaria(vetor, item, inicio, meio - 1);
+                else
+                    return pesquisaBinaria(vetor, item, meio + 1, fim);
+            }
+            else return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] vetor = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Posição do elemento procurado: " + pesquisaBinaria(vetor, 3, 0, vetor.Length - 1));
+            Console.ReadKey();
+        }
+    }
+}
